ec33a80fd9b1d151e2006693ab5ff6d0a8a41f9d
Jety16 <juan.carrillo.og@gmail.com>
2021-02-18 13:40:49 -0300

    Add Css

--- src/products/forms.py
+++ src/products/forms.py
@@ -2,6 +2,24 @@ from django import forms
 
 
 from .models import Product
+	
+class ProductForm(forms.ModelForm):
+	title		= forms.CharField(label="", widget=forms.TextInput(attrs={"placeholder":"Your Title"}))
+	description	= forms.CharField(
+									label='',	 
+									widget=forms.Textarea(attrs={
+										"rows":10,
+										"cols":120,
+										"placeholder":"Please, add a description"}
+										)
+									)
+	price		= forms.DecimalField(initial=0.0, )
+	class Meta:
+		model = Product
+		fields= [
+			'title',
+			'description',
+			'price']
 
 
 class RawProductForm(forms.Form):
